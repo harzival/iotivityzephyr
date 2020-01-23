c8e3f7140faa049f84f0900b3f854947385e5c3a
Ben Woodley <ben.woodley@unipart.io>
2020-01-23 12:29:47 +0000

    added docs

---
+++ uniport-docs/shell_variable_init.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export ZEPHYR_TOOLCHAIN_VARIANT="espressif"
+export ZEPHYR_SDK_INSTALL_DIR="${HOME}/zephyr-sdk-0.10.3"
+export ESPRESSIF_TOOLCHAIN_PATH="${HOME}/.espressif/tools/xtensa-esp32-elf/esp-2019r2-8.2.0/xtensa-esp32-elf"
+export ESP_IDF_PATH="${HOME}/dev/esp/esp-idf"
